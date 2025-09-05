7cc4bbac44ba9b2c6c4a06e840e4b5bd41cac989
Blaze Bryant <digital.entities@gmail.com>
2025-09-05 18:34:00 -0400

    Fixes for pytest environment variables that need to be loaded before django.setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,6 @@
 """Configuration file for pytest to set up the testing environment."""
 
-import os, sys
-import pytest
 from pytest_factoryboy import register
 from .factories import PostFactory
-
-
-@pytest.fixture(scope="session", autouse=True)
-def set_temp_env_var():
-    """Set temporary environment variables for testing."""
-
-    base_path = os.path.join(os.path.abspath(os.path.dirname(__name__)))
-    sys.path.append(os.path.join(base_path))
-
-    os.environ["SECRET_KEY"] = "test_secret_key"
-    os.environ["DEBUG"] = "True"
-
-    yield
-
 
 register(PostFactory)
